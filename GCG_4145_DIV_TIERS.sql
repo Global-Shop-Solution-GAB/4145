62b62918b4278ea2521c806bb861ad8796800e75
Service Web <sw@gssmail.com>
2024-03-11 13:36:24 -0500

    Commit changes in C:\Websites\Git\emarlow\4145\GCG_4145_DIV_TIERS.sql

---
+++ GCG_4145_DIV_TIERS.sql
@@ -0,0 +1,11 @@
+/* ==========================================
+ * TABLE: GCG_4145_DIV_TIERS
+ * ========================================== */
+CREATE TABLE "GCG_4145_DIV_TIERS"(
+ "TIER_ID" INTEGER,
+ "DIVISION_ID" INTEGER,
+ "GS_USER_ID" CHAR(8),
+ "TYPE_ID" INTEGER,
+ "TYPE_NAME" CHAR(50));
+
+
