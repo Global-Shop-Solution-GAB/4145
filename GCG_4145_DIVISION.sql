965ca47287856a7e24ded3cf8abb7c224550537d
Service Web <sw@gssmail.com>
2024-03-11 13:36:24 -0500

    Commit changes in C:\Websites\Git\emarlow\4145\GCG_4145_DIVISION.sql

---
+++ GCG_4145_DIVISION.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_4145_DIVISION
+ * ========================================== */
+CREATE TABLE "GCG_4145_DIVISION"(
+ "DIVISION_ID" INTEGER,
+ "DIVISION_NAME" CHAR(100));
+
+
