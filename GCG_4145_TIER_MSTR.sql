8496fb6ed377ef307c68f737b9a855e0166bbef8
Service Web <sw@gssmail.com>
2024-03-11 13:36:25 -0500

    Commit changes in C:\Websites\Git\emarlow\4145\GCG_4145_TIER_MSTR.sql

---
+++ GCG_4145_TIER_MSTR.sql
@@ -0,0 +1,9 @@
+/* ==========================================
+ * TABLE: GCG_4145_TIER_MSTR
+ * ========================================== */
+CREATE TABLE "GCG_4145_TIER_MSTR"(
+ "TIER_ID" INTEGER,
+ "MIN_TOTAL_PRICE" NUMERIC(10,2),
+ "MAX_TOTAL_PRICE" NUMERIC(10,2));
+
+
