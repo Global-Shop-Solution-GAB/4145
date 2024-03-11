af6d246d2574582d2de59d524b5b0569af83541c
Service Web <sw@gssmail.com>
2024-03-11 13:36:24 -0500

    Commit changes in C:\Websites\Git\emarlow\4145\GCG_4145_REQ_TYPE.sql

---
+++ GCG_4145_REQ_TYPE.sql
@@ -0,0 +1,8 @@
+/* ==========================================
+ * TABLE: GCG_4145_REQ_TYPE
+ * ========================================== */
+CREATE TABLE "GCG_4145_REQ_TYPE"(
+ "TYPE_ID" INTEGER,
+ "TYPE_NAME" CHAR(50));
+
+
